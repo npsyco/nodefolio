dacde0f83835d8f23c78048ef8132f8195b0652d
npsyco <npsyco@gmail.com>
2021-05-23 13:45:41 +0200

    fixed projects display, used jquesry script

--- public/projects/projects.js
+++ public/projects/projects.js
@@ -12,9 +12,9 @@ const { log } = console;
             $.each(data, function(i, projects) {
 
                 projects.forEach(function(value) {
-                    $('<div class="card-size">').appendTo("#projects")
-                    .append($("<h1></h1>").text(value.title))
-                    .append($("<h2></h2>").text(value.description))
+                    $('<div>').appendTo("#projects")
+                    .append($("<h2></h2>").text(value.title))
+                    .append($("<h3></h3>").text(value.description))
                     
                 });
             });
--- routes/projects.js
+++ routes/projects.js
@@ -5,23 +5,23 @@ const projects = [{
     description: "Personal portfolio implemented in Node.js",
     creationDate: new Date("2021-04-08"),
     /* endDate: new Date("2021-04-15"), */
-    gitLink: "https://github.com/npsyco/nodefolio.git"
+    /* gitLink: "https://github.com/npsyco/nodefolio.git" */
 }, 
 
 {
     title: "Battlesheet",
     description: "Attempted implementations of this diceroller in different languages, currently Java. Aim is to create a diceroller used in DnD games. Will attempt to code in Node.js next!",
-    creationDate: new Date("2020-08-01"),
+    /* creationDate: new Date("2020-08-01"), */
     /* endDate: Date.now, */
-    gitLink: "https://github.com/npsyco/BattleSheet.git",
+    /* gitLink: "https://github.com/npsyco/BattleSheet.git", */
 },
 
 {
     title: "Selinium AutoQA tester",
     description: "Experimenting with Selenium to automate website testing",
-    creationDate: new Date("2021-02-28"),
+    /* creationDate: new Date("2021-02-28"), */
     /* endDate: Date.now, */
-    gitLink: "https://github.com/npsyco/Selenium.git"
+    /* gitLink: "https://github.com/npsyco/Selenium.git" */
 }
 
 ];
